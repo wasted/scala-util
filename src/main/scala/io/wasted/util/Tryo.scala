5e09848a5db6f36aef5f79e8a8134ba8dd21ed6b
Franz Bettag <franz@bett.ag>
2012-12-06 01:10:05 +0100

    Updated Tryo to use Scala 2.10 Try().

--- src/main/scala/io/wasted/util/Tryo.scala
+++ src/main/scala/io/wasted/util/Tryo.scala
@@ -1,9 +1,11 @@
 package io.wasted.util
 
+import scala.util.{ Success, Failure }
+
 /**
  * This is a simple try/catch wrapped in an Option.
  */
 object Tryo {
-  def apply[T](f: => T): Option[T] = try { Some(f) } catch { case _: Throwable => None }
-  def apply[T](f: => T, fallback: T): T = try { f } catch { case _: Throwable => fallback }
+  def apply[T](f: => T): Option[T] = scala.util.Try(f) match { case Success(x) => Some(x) case _ => None }
+  def apply[T](f: => T, fallback: T): T = scala.util.Try(f) getOrElse fallback
 }
