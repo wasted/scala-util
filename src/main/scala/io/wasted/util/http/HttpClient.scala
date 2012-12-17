bfe43d4c294841ac4792a7fe38efe7a5776b31b3
Franz Bettag <franz@bett.ag>
2012-12-17 14:53:21 +0100

    Added thruput.io helper command.

--- src/main/scala/io/wasted/util/http/HttpClient.scala
+++ src/main/scala/io/wasted/util/http/HttpClient.scala
@@ -25,6 +25,16 @@ object HttpClient {
   /**
    * Creates a HTTP Client which will call the given method with the returned HttpResponse.
    *
+   * @param engine Optional SSLEngine
+   */
+  def apply(engine: Option[SSLEngine] = None): HttpClient[Object] = {
+    val doneF = (x: Option[io.netty.handler.codec.http.HttpResponse]) => {}
+    this.apply(new HttpClientResponseAdapter(doneF), engine)
+  }
+
+  /**
+   * Creates a HTTP Client which will call the given method with the returned HttpResponse.
+   *
    * @param doneF Function which will handle the result
    * @param engine Optional SSLEngine
    */
@@ -103,20 +113,21 @@ class HttpClient[T <: Object](handler: ChannelInboundMessageHandlerAdapter[T], e
 
     val channel = prepare(url).connect().sync().channel()
     channel.write(req)
-    channel.closeFuture().sync()
+    channel.closeFuture()
   }
 
   /**
-   * Send a Post-Request on the given URL.
+   * Send a PUT/POST-Request on the given URL with body.
    *
    * @param url This is getting weird..
-   * @param mime The MIME type the POST request
-   * @param body ByteArray to be POSTed
+   * @param mime The MIME type of the request
+   * @param body ByteArray to be send
+   * @param method HTTP Method to be used
    * @param headers
    */
-  def post(url: java.net.URL, mime: String, body: Array[Byte] = Array(), headers: Map[String, String] = Map()) {
-    val content = Unpooled.copiedBuffer(body)
-    val req = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, url.getPath)
+  def post(url: java.net.URL, mime: String, body: Seq[Byte] = Seq(), headers: Map[String, String] = Map(), method: HttpMethod) {
+    val content = Unpooled.copiedBuffer(body.toArray)
+    val req = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, url.getPath)
     req.setHeader(HttpHeaders.Names.HOST, url.getHost + ":" + getPort(url))
     req.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE)
     req.setHeader(HttpHeaders.Names.CONTENT_TYPE, mime)
@@ -125,8 +136,13 @@ class HttpClient[T <: Object](handler: ChannelInboundMessageHandlerAdapter[T], e
     req.setContent(content)
 
     val channel = prepare(url).connect().sync().channel()
-    channel.write(req).sync()
-    channel.closeFuture().sync()
+    channel.write(req)
+    channel.closeFuture()
+  }
+
+  def thruput(url: java.net.URL, auth: java.util.UUID, sign: java.util.UUID, payload: String) {
+    val headers = Map("X-Io-Auth" -> auth.toString, "X-Io-Sign" -> io.wasted.util.Hashing.sign(sign.toString, payload))
+    post(url, "application/json", payload.map(_.toByte), headers, HttpMethod.PUT)
   }
 }
 
