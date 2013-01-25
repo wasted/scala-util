4e80a6c8a04d7566161eefe25ed8f8e9a25f56c4
Franz Bettag <franz@bett.ag>
2013-01-25 02:48:25 +0100

    Added high-performance Thruput-via-WebSocket client.

--- src/main/scala/io/wasted/util/http/Thruput.scala
+++ src/main/scala/io/wasted/util/http/Thruput.scala
@@ -1,6 +1,6 @@
 package io.wasted.util.http
 
-import io.wasted.util.Logger
+import io.wasted.util._
 
 import io.netty.bootstrap._
 import io.netty.buffer._
@@ -15,18 +15,22 @@ import io.netty.util.CharsetUtil
 
 import javax.net.ssl.SSLEngine
 import java.net.InetSocketAddress
+import scala.util.{ Try, Success, Failure }
 
 /**
  * Thruput WebSocket Client class which will handle all delivery.
  */
 class Thruput(from: String, auth: java.util.UUID, sign: java.util.UUID, uri: java.net.URI, timeout: Int = 5, engine: Option[SSLEngine] = None) extends Logger {
 
-  private val session = java.util.UUID.randomUUID
+  private def session = java.util.UUID.randomUUID
+  private var channel: Option[Channel] = None
   private val handshaker = WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13, null, false, new DefaultHttpHeaders())
-  private val adapter = new ThruputResponseAdapter(handshaker)
+  private val adapter = new ThruputResponseAdapter(handshaker, this)
+  private var disconnecting = false
+  private var reconnecting = false
 
-  lazy val srv = new Bootstrap
-  lazy val bootstrap = srv.group(new NioEventLoopGroup)
+  private val srv = new Bootstrap()
+  private val bootstrap = srv.group(new NioEventLoopGroup)
     .channel(classOf[NioSocketChannel])
     .remoteAddress(new InetSocketAddress(uri.getHost, uri.getPort))
     .option[java.lang.Boolean](ChannelOption.TCP_NODELAY, true)
@@ -44,33 +48,67 @@ class Thruput(from: String, auth: java.util.UUID, sign: java.util.UUID, uri: jav
       }
     })
 
-  def connect(): Channel = {
-    val ch = bootstrap.connect().sync().channel()
-    adapter.handshakeFuture().sync()
-
-    val body = """{"from":"%s","thruput":true}""".format(from)
-    ch.write(new TextWebSocketFrame("""{"auth":"%s","sign":"%s","body":%s,"session":"%s"}""".format(
-      auth.toString, io.wasted.util.Hashing.sign(sign.toString, body), body, session.toString))).sync()
-    ch
+  def reconnect(): Unit = synchronized {
+    if (!reconnecting) {
+      reconnecting = true
+      disconnect()
+      connect()
+      reconnecting = false
+    }
   }
 
-  def disconnect(ch: Channel) {
-    ch.write(new CloseWebSocketFrame())
+  def connect() {
+    if (disconnecting) return
 
-    // WebSocketClientHandler will close the connection when the server
-    // responds to the CloseWebSocketFrame.
-    ch.closeFuture().sync()
+    channel match {
+      case Some(ch) => debug("Connection already established")
+      case _ =>
+        val ch = bootstrap.duplicate.connect().sync().channel()
+        adapter.handshakeFuture().sync()
+
+        val body = """{"from":"%s","thruput":true}""".format(from)
+        ch.write(new TextWebSocketFrame("""{"auth":"%s","sign":"%s","body":%s,"session":"%s"}""".format(
+          auth.toString, io.wasted.util.Hashing.sign(sign.toString, body), body, session.toString))).sync()
+
+        channel = Some(ch)
+    }
   }
 
-  def close() {
+  def disconnect() {
+    disconnecting = true
+    channel match {
+      case Some(ch) =>
+        ch.flush
+        ch.write(new CloseWebSocketFrame())
+
+        // WebSocketClientHandler will close the connection when the server
+        // responds to the CloseWebSocketFrame.
+        ch.closeFuture().sync()
+      case _ => debug("Connection not established")
+    }
+    channel = None
+    disconnecting = false
+  }
+
+  def write(wsf: WebSocketFrame) {
+    channel match {
+      case Some(ch) =>
+        ch.write(wsf)
+      case _ => debug("Connection not established")
+    }
+  }
+
+  def shutdown() {
     srv.shutdown()
   }
+
 }
 
 /**
  * Empty Netty Response Adapter which is used for Thruput high-performance delivery.
  */
-class ThruputResponseAdapter(handshaker: WebSocketClientHandshaker) extends ChannelInboundMessageHandlerAdapter[Object] with Logger {
+@ChannelHandler.Sharable
+class ThruputResponseAdapter(handshaker: WebSocketClientHandshaker, client: Thruput) extends ChannelInboundMessageHandlerAdapter[Object] with Logger {
   private var _handshakeFuture: ChannelPromise = null.asInstanceOf[ChannelPromise]
   def handshakeFuture() = _handshakeFuture
 
@@ -83,31 +121,29 @@ class ThruputResponseAdapter(handshaker: WebSocketClientHandshaker) extends Chan
   }
 
   override def channelInactive(ctx: ChannelHandlerContext) {
-    warn("WebSocket Client disconnected!")
+    info("WebSocket Client disconnected!")
+    client.reconnect
   }
 
   override def messageReceived(ctx: ChannelHandlerContext, msg: Object) {
     val ch = ctx.channel()
-    if (!handshaker.isHandshakeComplete()) {
-      handshaker.finishHandshake(ch, msg.asInstanceOf[FullHttpResponse])
-      warn("WebSocket Client connected!")
-      handshakeFuture.setSuccess()
-      return
-    }
 
     msg match {
+      case response: FullHttpResponse if !handshaker.isHandshakeComplete =>
+        handshaker.finishHandshake(ch, response)
+        info("WebSocket Client connected!")
+        handshakeFuture.setSuccess()
       case response: FullHttpResponse =>
-        throw new Exception("Unexpected FullHttpResponse (status=" + response.status() +
-          ", content=" + response.data().toString(CharsetUtil.UTF_8) + ')');
+        throw new Exception("Unexpected FullHttpResponse (status=" + response.status() + ", content=" + response.data().toString(CharsetUtil.UTF_8) + ")")
       case frame: TextWebSocketFrame =>
-        warn("WebSocket Client received message: " + frame.text())
+        debug("WebSocket Client received message: " + frame.text())
       case frame: PongWebSocketFrame =>
-        warn("WebSocket Client received pong")
+        debug("WebSocket Client received pong")
       case frame: CloseWebSocketFrame =>
-        warn("WebSocket Client received closing")
+        debug("WebSocket Client received closing")
         ch.close()
-      case _ =>
-        error("Unsupported response type!")
+      case o: Object =>
+        error("Unsupported response type! " + o.toString)
         ch.close()
     }
   }
@@ -116,5 +152,6 @@ class ThruputResponseAdapter(handshaker: WebSocketClientHandshaker) extends Chan
     cause.printStackTrace //ExceptionHandler(ctx, cause)
     if (!handshakeFuture.isDone()) handshakeFuture.setFailure(cause)
     ctx.close()
+    client.reconnect
   }
 }
