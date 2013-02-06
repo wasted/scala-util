028d8c9144426ac8aa874717a193df5f134160a2
Franz Bettag <franz@bett.ag>
2013-02-06 16:50:45 +0100

    Logger performance fixes.

--- src/main/scala/io/wasted/util/Logger.scala
+++ src/main/scala/io/wasted/util/Logger.scala
@@ -32,6 +32,7 @@ trait Logger {
    * Prints a message on debug.
    */
   def debug(msg: String, x: Any*) {
+    if (!logger.isDebugEnabled()) return
     x.foreach(_ match { case msg: Throwable => submitException(msg) case _ => })
     logger.debug(msg.format(x: _*))
   }
@@ -40,6 +41,7 @@ trait Logger {
    * Prints a message on info.
    */
   def info(msg: String, x: Any*) {
+    if (!logger.isInfoEnabled()) return
     x.foreach(_ match { case msg: Throwable => submitException(msg) case _ => })
     logger.info(msg.format(x: _*))
   }
@@ -48,6 +50,7 @@ trait Logger {
    * Prints a message on warn.
    */
   def warn(msg: String, x: Any*) {
+    if (!logger.isWarnEnabled()) return
     x.foreach(_ match { case msg: Throwable => submitException(msg) case _ => })
     logger.warn(msg.format(x: _*))
   }
@@ -56,6 +59,7 @@ trait Logger {
    * Prints a message on error.
    */
   def error(msg: String, x: Any*) {
+    if (!logger.isErrorEnabled()) return
     x.foreach(_ match { case msg: Throwable => submitException(msg) case _ => })
     logger.error(msg.format(x: _*))
   }
