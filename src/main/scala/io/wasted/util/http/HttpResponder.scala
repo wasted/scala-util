c65940123e3ec137704da1fdecdd781cca00bb20
Franz Bettag <franz@bett.ag>
2014-01-26 17:56:20 +0100

    Added HTTP helpers.

---
+++ src/main/scala/io/wasted/util/http/HttpResponder.scala
@@ -0,0 +1,51 @@
+package io.wasted.util.http
+
+import io.netty.buffer._
+import io.netty.handler.codec.http._
+import io.netty.handler.codec.http.HttpHeaders._
+import io.netty.handler.codec.http.HttpHeaders.Names._
+import io.netty.handler.codec.http.HttpVersion._
+
+/**
+ * Responder class used in our server applications
+ * @param token HTTP Server token
+ * @param pooledBuffer If we use pooled netty buffers
+ * @param heapBuffer or heap buffers (pooled wins over this)
+ */
+class HttpResponder(token: String, pooledBuffer: Boolean, heapBuffer: Boolean) {
+  def apply(
+    status: HttpResponseStatus,
+    body: Option[String] = None,
+    mime: Option[String] = None,
+    close: Boolean = true,
+    headers: Map[String, String] = Map()): FullHttpResponse = {
+    val res = body match {
+      case Some(body) =>
+        val bytes = body.getBytes("UTF-8").toArray
+        val content = pooledBuffer match {
+          case true =>
+            val pooledBuf = heapBuffer match {
+              case true => PooledByteBufAllocator.DEFAULT.heapBuffer(bytes.length, bytes.length)
+              case flase => PooledByteBufAllocator.DEFAULT.directBuffer(bytes.length, bytes.length)
+            }
+            pooledBuf.setBytes(0, bytes)
+            pooledBuf
+          case false => Unpooled.wrappedBuffer(bytes)
+        }
+        val res = new DefaultFullHttpResponse(HTTP_1_1, status, content)
+        setContentLength(res, content.readableBytes())
+        res
+      case None =>
+        val res = new DefaultFullHttpResponse(HTTP_1_1, status)
+        setContentLength(res, 0)
+        res
+    }
+
+    mime.map(res.headers.set(CONTENT_TYPE, _))
+    res.headers.set(SERVER, token)
+    headers.foreach { h => res.headers.set(h._1, h._2) }
+
+    res.headers.set(CONNECTION, if (close) Values.CLOSE else Values.KEEP_ALIVE)
+    res
+  }
+}
